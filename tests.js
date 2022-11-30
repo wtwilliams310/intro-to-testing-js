45f9e484846a3d0bf9d5d12484138922b80264bb
Titus Williams <wtwilliams310@gmail.com>
2022-11-30 15:54:54 -0600

    completed part 1 exercise#1

--- tests.js
+++ tests.js
@@ -13,3 +13,8 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+});
